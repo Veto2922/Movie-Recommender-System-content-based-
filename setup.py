e6b7a545c0a5e5eca2209f4e66e62cf487d7c023
Abdelrahman mohamed <114834171+Veto2922@users.noreply.github.com>
2024-01-16 21:04:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-with open('README.md' , 'r' , encoding='utf-8' ) as f:
-    long_description = f.read()
+# with open('README.md' , 'r' , encoding='utf-8' ) as f:
+#     long_description = f.read()
 
 setup(
     name='src',
@@ -10,8 +10,8 @@ setup(
     description='This project aims to build a movie recommendation system using data on genres,, cast, crew, overview, release_year , runtime , keywords , vote_average , Director of several thousand films. It combines features such as cosine similarity and Euclidean distance to provide movie recommendations.',
     author='Abdelrahman mohamed',
     license='MIT',
-    long_description= long_description,
-    packages=['src'],
+    # long_description= long_description,
+    # packages=['src'],
     python_requires = '>=3.10.8',
-    install_requires = ['streamlit']
+    # install_requires = ['streamlit']
 )
